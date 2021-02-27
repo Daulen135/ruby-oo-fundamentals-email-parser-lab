d0de9e43a27165f90bd5c8e0babece387b1385e6
Daulen135 <daulenz@gmail.com>
2021-02-27 13:01:13 -0600

    commit tree 14e2be03f31d6a434234f94b705a6406d51b31b8

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -2,3 +2,38 @@
 # emails. The parse method on the class should separate them into
 # unique email addresses. The delimiters to support are commas (',')
 # or whitespace (' ').
+
+require "pry"
+class EmailAddressParser 
+    attr_accessor :e_mail
+    
+    def initialize(e_mail)
+        @e_mail=e_mail
+    end
+    
+    def parse
+        @e_mail.split(/[,\s]+/).uniq #string regex method
+    end
+end
+
+# emails = "abc@gmail.com, abcd@gmail.com"
+# parser = EmailAddressParser.new(emails)
+# parser.parse
+
+#class EmailAddressParser 
+ #   attr_accessor :parse
+#end
+# print EmailAddressParser.new.parse.split(",")
+
+
+
+
+
+#print EmailAddressParser.new.parse.split(",")
+
+
+#daulen=EmailAddressParser.new
+#daulen.parse="a,b"
+#print daulen.parse.split(",")
+#binding.pry
+#Q
